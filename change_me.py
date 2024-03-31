758af59176188b17ee68cabdcfc905596b0a0cd1
jmdelong7 <62762261+jmdelong7@users.noreply.github.com>
2024-03-30 17:31:21 -0700

    Create change_me.py

---
+++ change_me.py
@@ -0,0 +1,20 @@
+# Original raw data file location
+excel_file = r"C:\Users\djaco\Desktop\ebay\data\data1.xlsx"
+
+# New data file location
+excel_file_new = r"C:\Users\djaco\Desktop\ebay\data\data1_new.xlsx"
+
+# Photos folder location
+photos_folder = r"C:\Users\djaco\Desktop\ebay\photos"
+
+# HTML description file location
+html_description_file = r"C:\Users\djaco\Desktop\ebay\description_template.html"
+
+# Default text if a pre-owned item has no flaws (otherwise it returns the condition_description).
+default = "No flaws."
+
+# Time delay between actions in seconds
+time_sleep = 1
+
+# Page load delay in seconds
+page_delay = 5
