8f02c02ec5551b14b697f3b8f4fb0364645a18d8
jmdelong7 <62762261+jmdelong7@users.noreply.github.com>
2024-04-20 19:59:53 -0700

    Update change_me.py

--- change_me.py
+++ change_me.py
@@ -1,20 +1,21 @@
-# Excel file location after done taking photos
+# Excel file location after done taking photos.
 excel_file = r"C:\Users\djaco\Desktop\ebay\data\data1.xlsx"
 
-# New data file location, this is the file that the script will use to transfer data to ebay listings
+# New data file location, this is the name of the new file and its path. 
+# The listing script (list.py) will also look here to use it.
 excel_file_new = r"C:\Users\djaco\Desktop\ebay\data\data1_new.xlsx"
 
-# Folder where all the photos are for the current listing session
+# Folder where all the photos are for the current listing session.
 photos_folder = r"C:\Users\djaco\Desktop\ebay\photos"
 
-# HTML description file location
+# HTML description file location.
 html_description_file = r"C:\Users\djaco\Desktop\ebay\description_template.html"
 
 # Default text if a pre-owned item has no flaws (otherwise it returns the condition_description).
 default = "No flaws."
 
-# Time delay between actions in seconds
+# Time delay between actions in seconds.
 time_sleep = 1
 
-# Time to wait for page to load in seconds
+# Time to wait for page to load in seconds.
 page_delay = 5
